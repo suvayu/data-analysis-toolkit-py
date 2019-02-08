e9005a165d37c4ed74fed9d153ed44317eb5db19
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2019-02-09 01:22:08 +0530

    utils/iter: consume iterators (copy & paste from itertools docs)

--- datk/utils/iter.py
+++ datk/utils/iter.py
@@ -77,6 +77,20 @@ class LookAheadItr(object):
         return self
 
 
+def consume(iterator, n=None):
+    """Advance the iterator n-steps ahead. If n is None, consume entirely.
+
+    source: "Itertools Recipes" in itertools docs
+    """
+    # Use functions that consume iterators at C speed.
+    if n is None:
+        # feed the entire iterator into a zero-length deque
+        collections.deque(iterator, maxlen=0)
+    else:
+        # advance to the empty slice starting at position n
+        next(islice(iterator, n, n), None)
+
+
 def call_hook(hook):
     """Call the hook function"""
     try:
